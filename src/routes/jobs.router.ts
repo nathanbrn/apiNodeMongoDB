b00ec6f1003d5dc5f771d903ceb048e34628c3f5
Breno Nathanael <breno_nathanael@hotmail.com>
2023-05-27 11:57:10 -0300

    criação de rota para novo serviço

--- src/routes/jobs.router.ts
+++ src/routes/jobs.router.ts
@@ -3,13 +3,19 @@ import { findJobs } from "../useCases/JobCases/findJobs";
 import { createJob } from "../useCases/JobCases/createJob";
 import { updateJob } from "../useCases/JobCases/updateJob";
 import { deleteJob } from "../useCases/JobCases/deleteJob";
+import { findJob } from "../useCases/JobCases/findJob";
+import { findJobStars } from "../useCases/JobCases/findJobStars";
 
 export const jobRouter = Router();
 
 jobRouter.get('/jobs', findJobs);
+
+jobRouter.get('/jobs/:id', findJob);
 
 jobRouter.post('/jobs', createJob);
 
 jobRouter.put('/jobs/:id', updateJob);
 
 jobRouter.delete('/jobs/:id', deleteJob);
+
+jobRouter.get('/starjobs', findJobStars);
