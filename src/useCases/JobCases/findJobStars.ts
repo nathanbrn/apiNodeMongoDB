8c3ed42e54489480bb1219d309520695678cd28c
Breno Nathanael <breno_nathanael@hotmail.com>
2023-05-27 11:56:42 -0300

    criação de novo serviço

---
+++ src/useCases/JobCases/findJobStars.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { Job } from "../../models/Job";
+
+export async function findJobStars(req: Request, res: Response) {
+  try {
+    const jobs = await Job.find({ star: true});
+
+    if (jobs.length === 0) {
+      return res.status(404).json({ message: "Vagas não encontradas" });
+    }
+
+    res.json(jobs);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
