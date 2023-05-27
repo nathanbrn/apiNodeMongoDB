3b54a8bc89b8279eda4df5a05208f2e435328591
Breno Nathanael <breno_nathanael@hotmail.com>
2023-05-27 11:29:44 -0300

    rota de listar uma vaga

---
+++ src/useCases/JobCases/findJob.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { Job } from "../../models/Job";
+
+export async function findJob(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+
+    const job = await Job.findById(id);
+
+    if (!job) {
+      return res.status(404).json({ message: 'Job não encontrado' });
+    }
+
+    res.json(job);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Erro ao buscar Job' });
+  }
+}
