75791482ae003407c4b3b67184fd349d92f3b783
Breno Nathanael <breno_nathanael@hotmail.com>
2023-05-27 11:29:08 -0300

    update em alguns metodos

--- src/useCases/JobCases/deleteJob.ts
+++ src/useCases/JobCases/deleteJob.ts
@@ -2,13 +2,18 @@ import { Request, Response } from "express";
 import { Job } from "../../models/Job";
 
 export async function deleteJob(req: Request, res: Response) {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  if (!id) {
-    return res.status(404).send('User not found!');
+    if (!id) {
+      return res.status(404).send('User not found!');
+    }
+
+    await Job.deleteOne({ _id: id });
+
+    res.status(204).json({});
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send('Erro ao deletar vaga');
   }
-
-  await Job.deleteOne({ _id: id });
-
-  res.status(204).json({});
 }
--- src/useCases/JobCases/findJobs.ts
+++ src/useCases/JobCases/findJobs.ts
@@ -5,9 +5,13 @@ export async function findJobs(req: Request, res: Response) {
  try {
   const jobs = await Job.find();
 
+  if (jobs.length === 0) {
+   return res.status(404).json({ message: 'Nenhuma vaga encontrada' });
+  }
+
   res.status(200).json(jobs);
  } catch (err) {
-  res.status(500).send('Erro ao listar jobs');
   console.log(err);
+  res.status(500).send('Erro ao listar jobs');
  }
 }
