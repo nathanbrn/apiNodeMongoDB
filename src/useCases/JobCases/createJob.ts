7c534496ab72c9c43b4793be051e598b35851b9b
Breno Nathanael <breno_nathanael@hotmail.com>
2023-05-27 11:29:31 -0300

    alteração: preenchimento do campo star

--- src/useCases/JobCases/createJob.ts
+++ src/useCases/JobCases/createJob.ts
@@ -3,23 +3,50 @@ import { Job } from '../../models/Job';
 
 export async function createJob(req: Request, res: Response) {
 	try {
-		const { title, hour, salary, exp, link } = req.body;
+		const {
+      title,
+      experience,
+      enterprice,
+      type,
+      hour,
+      salary,
+      location,
+      description,
+      requirements,
+      differentials,
+      benefits,
+      url,
+     } = req.body;
 
-		if (!title || !link) {
+     let star = false;
+
+		if (!title || !experience || !enterprice || !location || !requirements || !url) {
 			return res.status(400).send('Dados obrigatórios não foram preenchidos');
 		}
 
+    if (title && experience && enterprice && type && hour && salary && location && description && requirements && differentials && benefits && url) {
+      star = true;
+    }
+
 		const job = await Job.create({
 			title,
-			hour,
-			salary,
-			exp,
-			link
+      experience,
+      enterprice,
+      type,
+      hour,
+      salary,
+      location,
+      description,
+      requirements,
+      differentials,
+      benefits,
+      url,
+      star
 		});
 
 		res.status(201).json(job);
 	} catch (err) {
-		return res.status(400).send('Erro ao criar vaga');
 		console.log(err);
+		return res.status(400).send('Erro ao criar vaga');
 	}
 }
